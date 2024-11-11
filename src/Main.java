a9187ad02e3f8410a2354113bcfbddfb69622fde
LucasACH <achaval.lucas@gmail.com>
2024-11-11 20:50:04 -0300

    Add Main class to execute all test cases

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        tests.TestCellCompare.main(args);
+        tests.TestDataFrame.main(args);
+        tests.TestFilter.main(args);
+        tests.TestGroupedDataFrame.main(args);
+        tests.TestReadCSV.main(args);
+        tests.TestReadJSON.main(args);
+        tests.TestSortBy.main(args);
+    }
+}
