ebfa02fd8f9519f44152a21f3b6f289ec1d68dd1
LucasACH <achaval.lucas@gmail.com>
2024-11-12 20:11:08 -0300

    Update generated documentation timestamps and add performance section to element list

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"exceptions"},{"l":"interfaces"},{"l":"libraries"},{"l":"structures"},{"l":"tests"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"exceptions"},{"l":"interfaces"},{"l":"libraries"},{"l":"performance"},{"l":"structures"},{"l":"tests"}];updateSearchResults();
